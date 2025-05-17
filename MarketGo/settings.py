d149daaad20ed69b52fe016fa0ad48dc62a0fbc9
hamza <hamzaitmoha@gmail.com>
2025-05-17 13:29:45 +0000

    Ajout de la configuration ArgoCD

--- MarketGo/settings.py
+++ MarketGo/settings.py
@@ -94,16 +94,18 @@ pymysql.install_as_MySQLdb()
 
 
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'marketgo_db',
         'USER': 'marketgo_user',
         'PASSWORD': 'marketgo_pass',
-        'HOST': 'db',  # Correspond au nom du service dans docker-compose.yml
+        'HOST': 'mysql-service',  # Bien le nom du service Kubernetes MySQL
         'PORT': '3306',
     }
 }
+
 
 
 # Password validation
